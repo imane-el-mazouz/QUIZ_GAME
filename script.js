3c29a6a14452c0f02f99229a7c15e47d7fb9842d
iima23 <elmazouz.imane@gmail.com>
2024-03-12 15:37:25 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+/*onload = function (){
+    document.querySelector('h1').style.color='red';
+
+}*/
+/*document.write('hey');
+console.log('hey');*/
+/*window.alert('hello');*/
+/*window.document.write("WORLD");*/
+//document.write('<p>test</p>');
+//document.write('<p>test</p>');
+
+//document.write(typeof x);//undefined
+
+//let y=4;
+//document.write(typeof y);
+//var a;
+//a=3;
+//console.log(a);
+//let x ='im';
+/*console.log(x);
+console.log(x);
+console.log(x);
+console.log(x);*/
+//let name= "im";
+//console.log(name.repeat(1000));
+//let name = 'ali';
+//name = 'a';
+//name = 'l';
+//name = 'i';
+//console.log(name.length);
+//let name = 'imane ';
+//console.log(name indexof('o'))
+
+
+
+
+
+
